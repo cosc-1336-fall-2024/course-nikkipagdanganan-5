a7c0a9c97427fb215d1e971c4e2eea59b464a709
nikkipagdanganan <nikki.pagdanganan@g.austincc.edu>
2024-09-20 18:31:24 +0000

    homework3

--- tests/examples/d_repetition/tests_repetition.py
+++ tests/examples/d_repetition/tests_repetition.py
@@ -1,9 +1,21 @@
 import unittest
 
-from src.examples.d_repetition.repetition import test_config
+from src.homework.d_repetition.repetition import test_config
+from src.homework.d_repetition.repetition import get_factorial
+from src.homework.d_repetition.repetition import sum_odd_numbers
 
 class Test_Config(unittest.TestCase):
 
     def test_configuration(self):
         self.assertEqual(True, test_config())
+
+    def test_get_factorial(self):
+        self.assertEqual(24, get_factorial(4))
+        self.assertEqual(120, get_factorial(5))
+        self.assertEqual(720, get_factorial(6))
+
+    def test_sum_odd_numbers(self):
+        self.assertEqual(16, sum_odd_numbers(7))
+        self.assertEqual(25, sum_odd_numbers(9))
+        self.assertEqual(25, sum_odd_numbers(10))
 
